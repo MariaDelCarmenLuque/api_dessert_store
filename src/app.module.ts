e3856654c4c09a4df6280797698bb0089563d3b5
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-05-16 09:52:01 -0500

    feat: Added categories module in import of app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,15 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { DessertsModule } from './desserts/desserts.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
   imports: [
     UsersModule,
     AuthModule,
-    EventEmitterModule.forRoot(),
+    CategoriesModule,
     DessertsModule,
+    EventEmitterModule.forRoot(),
   ],
   controllers: [AppController],
   providers: [AppService],
