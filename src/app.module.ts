494c01870f14110d6e80fea0476f91e94bb5befd
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-05-16 06:34:41 -0500

    fix: resolved linter probles in app.nodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { EventEmitterModule } from '@nestjs/event-emitter';
+import { DessertsModule } from './desserts/desserts.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, EventEmitterModule.forRoot()],
+  imports: [
+    UsersModule,
+    AuthModule,
+    EventEmitterModule.forRoot(),
+    DessertsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
