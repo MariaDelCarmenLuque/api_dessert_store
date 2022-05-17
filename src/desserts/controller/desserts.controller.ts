41e8d4a3b4958399e7d05931c096545f9eba226d
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-05-17 16:03:43 -0500

    feat: Added jwtModule config

--- src/desserts/controller/desserts.controller.ts
+++ src/desserts/controller/desserts.controller.ts
@@ -3,7 +3,9 @@ import {
   Controller,
   DefaultValuePipe,
   Delete,
+  ForbiddenException,
   Get,
+  HttpCode,
   HttpException,
   HttpStatus,
   Param,
@@ -11,18 +13,85 @@ import {
   Patch,
   Post,
   Query,
+  UnauthorizedException,
+  UseGuards,
 } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiConflictResponse,
+  ApiForbiddenResponse,
+  ApiInternalServerErrorResponse,
+  ApiNotFoundResponse,
+  ApiOperation,
+  ApiQuery,
+  ApiResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 import { Dessert } from '@prisma/client';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Public } from 'src/auth/public.decorator';
+import { Roles } from 'src/auth/roles.decorator';
+import { Role } from 'src/auth/roles.enum';
 import { CreateDessertDto } from '../models/create-dessert.dto';
 import { DessertDto } from '../models/dessert.dto';
 import { UpdateDessertDto } from '../models/update-dessert.dto';
 import { DessertsService } from '../service/desserts.service';
 
+@ApiTags('Desserts')
 @Controller('desserts')
 export class DessertsController {
   constructor(private dessertsService: DessertsService) {}
 
   @Get()
+  @Public()
+  @ApiOperation({ summary: 'Get all Desserts with optional filters' })
+  @ApiResponse({
+    status: 200,
+    description: 'Return a list of dessert, search by category ',
+    schema: {
+      example: {
+        items: [
+          {
+            id: 2,
+            uuid: 'ea8314b6-7c5f-44eb-8308-73c6ddbbcc90',
+            name: 'Chocolate cake',
+            description:
+              'A cake with many variety of dried fruits and chocolates',
+            price: 250,
+            stock: 14,
+            status: 'ACTIVE',
+            categoryId: 1,
+            deletedAt: null,
+            updatedAt: '2022-05-17T01:34:30.374Z',
+            createdAt: '2022-05-17T01:34:30.372Z',
+          },
+          {
+            id: 1,
+            uuid: '5159568b-71d7-4c81-b428-184241963655',
+            name: 'Orange cake',
+            description: 'A cake with citric fruits',
+            price: 100,
+            stock: 10,
+            status: 'ACTIVE',
+            categoryId: 1,
+            deletedAt: null,
+            updatedAt: '2022-05-17T16:38:07.892Z',
+            createdAt: '2022-05-17T16:38:07.889Z',
+          },
+        ],
+      },
+    },
+  })
+  @ApiQuery({
+    name: 'category',
+    type: 'number',
+    description: 'Category ID',
+    required: false,
+    example: 1,
+  })
   async getAllDesserts(
     @Query('take', new DefaultValuePipe(10), ParseIntPipe) take,
     @Query('skip', new DefaultValuePipe(1), ParseIntPipe) skip,
@@ -34,15 +103,123 @@ export class DessertsController {
   }
 
   @Get('/:id')
+  @Public()
+  @HttpCode(200)
+  @ApiOperation({ summary: 'Get a dessert by ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'Dessert found by ID',
+    schema: {
+      example: {
+        id: 1,
+        uuid: '5159568b-71d7-4c81-b428-184241963655',
+        name: 'Orange cake',
+        description: 'A cake with citric fruits',
+        price: 100,
+        stock: 10,
+        status: 'ACTIVE',
+        categoryId: 1,
+        deletedAt: null,
+        updatedAt: '2022-05-17T16:38:07.892Z',
+        createdAt: '2022-05-17T16:38:07.889Z',
+      },
+    },
+  })
+  @ApiNotFoundResponse({
+    description: 'Dessert Not Found',
+    schema: {
+      example: {
+        statusCode: 404,
+        message: 'Dessert with id 12344 Not found',
+      },
+    },
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal Server Error',
+    schema: {
+      example: {
+        statusCode: 500,
+        message: 'Internal server error',
+      },
+    },
+  })
   async findDessertById(@Param('id') id: number): Promise<Dessert> {
     const dessert = await this.dessertsService.findOne(id);
     if (!dessert) {
-      throw new HttpException('User Not Found', HttpStatus.NOT_FOUND);
+      throw new HttpException(
+        `Dessert with id ${id} Not Found`,
+        HttpStatus.NOT_FOUND,
+      );
     }
     return dessert;
   }
 
   @Post()
+  @ApiBearerAuth()
+  @Roles(Role.ADMIN)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @ApiOperation({ summary: 'Create a new Dessert' })
+  @ApiBody({
+    schema: {
+      example: {
+        name: 'Banana cake',
+        description: 'A cake with banana and chips of chocolates',
+        stock: 100,
+        price: 200,
+        categoryId: 2,
+      },
+    },
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Create a newdessert',
+    schema: {
+      example: {
+        id: 4,
+        uuid: '4ae80f89-4f6d-4aeb-80ca-5c6403b74c6a',
+        name: 'Banana cake',
+        description: 'A cake with banana and chips of chocolates',
+        price: 200,
+        stock: 100,
+        categoryId: 2,
+        status: 'ACTIVE',
+        images: [],
+        updatedAt: '2022-05-17T19:09:16.174Z',
+        createdAt: '2022-05-17T19:09:16.172Z',
+      },
+    },
+  })
+  @ApiNotFoundResponse({
+    description: 'Category not found',
+    schema: {
+      example: {
+        statusCode: 404,
+        message: 'Category not found',
+        error: 'Not Found',
+      },
+    },
+  })
+  @ApiUnauthorizedResponse({
+    schema: {
+      example: new UnauthorizedException().getResponse(),
+    },
+    description: 'User is not logged in',
+  })
+  @ApiForbiddenResponse({
+    description: 'User is not authorized to delete this product',
+    schema: {
+      example: new ForbiddenException().getResponse(),
+    },
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal Server Error',
+    schema: {
+      example: {
+        statusCode: 500,
+        message: 'Internal server error',
+      },
+    },
+  })
   async createDessert(
     @Body() createDessert: CreateDessertDto,
   ): Promise<DessertDto> {
@@ -50,6 +227,88 @@ export class DessertsController {
   }
 
   @Patch('/:id')
+  @ApiBearerAuth()
+  @Roles(Role.ADMIN)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @ApiOperation({ summary: 'Update or create a dessert ' })
+  @ApiBody({
+    schema: {
+      example: {
+        name: 'Banana cake',
+        description: 'A banana cake with chocolate',
+      },
+    },
+    type: UpdateDessertDto,
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Create a new Dessert',
+    schema: {
+      example: {
+        id: 4,
+        uuid: '4ae80f89-4f6d-4aeb-80ca-5c6403b74c6a',
+        name: 'Banana cake',
+        description: 'A cake with banana and chips of chocolates',
+        price: 200,
+        stock: 100,
+        categoryId: 2,
+        status: 'ACTIVE',
+        images: [],
+        updatedAt: '2022-05-17T19:09:16.174Z',
+        createdAt: '2022-05-17T19:09:16.172Z',
+      },
+    },
+  })
+  @ApiNotFoundResponse({
+    description: 'Dessert not found',
+    schema: {
+      example: {
+        statusCode: 404,
+        message: 'Dessert with id 123 Not Found',
+        error: 'Not Found',
+      },
+    },
+  })
+  @ApiConflictResponse({
+    description: 'One or more properties are missing or are wrong',
+    schema: {
+      example: {
+        statusCode: 400,
+        message: 'Unexpected token m in JSON at position 18',
+        error: 'Bad Request',
+      },
+    },
+  })
+  @ApiUnauthorizedResponse({
+    schema: {
+      example: {
+        statusCode: 401,
+        message: 'Dessert is disable ',
+        error: 'Unauthorized',
+      },
+    },
+  })
+  @ApiUnauthorizedResponse({
+    schema: {
+      example: new UnauthorizedException().getResponse(),
+    },
+    description: 'User is not logged in',
+  })
+  @ApiForbiddenResponse({
+    description: 'User is not authorized to delete this product',
+    schema: {
+      example: new ForbiddenException().getResponse(),
+    },
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal Server Error',
+    schema: {
+      example: {
+        statusCode: 500,
+        message: 'Internal server error',
+      },
+    },
+  })
   async updateDessert(
     @Param('id') id: number,
     @Body() updateDessert: UpdateDessertDto,
@@ -58,16 +317,83 @@ export class DessertsController {
   }
 
   @Delete('/:id')
+  // @ApiBearerAuth()
+  // @Roles(Role.ADMIN)
+  // @UseGuards(JwtAuthGuard, RolesGuard)
+  @ApiOperation({ summary: 'Delete a product by ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'Dessert deleted sucessfully',
+    schema: {
+      example: '200',
+    },
+  })
+  @ApiUnauthorizedResponse({
+    schema: {
+      example: new UnauthorizedException().getResponse(),
+    },
+    description: 'User is not logged in',
+  })
+  @ApiForbiddenResponse({
+    description: 'User is not authorized to delete this product',
+    schema: {
+      example: new ForbiddenException().getResponse(),
+    },
+  })
+  @ApiNotFoundResponse({
+    description: 'Dessert not found',
+    schema: {
+      example: {
+        statusCode: 404,
+        message: 'Dessert with id 123 Not Found',
+        error: 'Not Found',
+      },
+    },
+  })
   async delete(@Param('id') id: number) {
     return await this.dessertsService.deleteDessert(id);
   }
 
   @Patch('/:id/status')
+  @ApiBearerAuth()
+  @Roles(Role.ADMIN)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @ApiOperation({ summary: 'Disable or active a dessert by ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'Update dessert status',
+    type: DessertDto,
+    schema: {
+      example: {
+        id: 2,
+        name: 'Chocolate cake (update)',
+        status: 'DISABLE',
+        updatedAt: '2022-05-17T01:34:30.374Z',
+      },
+    },
+  })
+  @ApiNotFoundResponse({
+    description: 'Dessert not found',
+    schema: {
+      example: {
+        statusCode: 404,
+        message: 'Dessert with id 15 Not found',
+      },
+    },
+  })
+  @ApiUnauthorizedResponse({
+    schema: {
+      example: new UnauthorizedException().getResponse(),
+    },
+    description: 'User is not logged in',
+  })
+  @ApiForbiddenResponse({
+    description: 'User is not authorized to delete this product',
+    schema: {
+      example: new ForbiddenException().getResponse(),
+    },
+  })
   async enableStatus(@Param('id') id: number) {
-    // const dessert: Dessert = await this.dessertsService.findOne(id);
-    // if (!dessert) {
-    //   throw new HttpException('Dessert Not Found', HttpStatus.NOT_FOUND);
-    // }
     return await this.dessertsService.updateStatus(id);
   }
 }
