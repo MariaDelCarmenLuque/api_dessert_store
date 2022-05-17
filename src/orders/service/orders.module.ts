acd552a2835c3971ad10657f778173d3030a073b
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-05-17 07:46:50 -0500

    fix: Removed order.module of service file

--- src/orders/service/orders.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { OrdersController } from '../controller/orders.controller';
-import { OrdersService } from '../orders.service';
-
-@Module({
-  controllers: [OrdersController],
-  providers: [OrdersService],
-})
-export class OrdersModule {}
