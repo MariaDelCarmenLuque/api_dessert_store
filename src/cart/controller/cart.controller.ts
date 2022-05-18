9b2736244b15787ad9cfbd18b73169aad9e34d27
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-05-18 00:22:54 -0500

    feat: Added swagger documentation in cart controller

--- src/cart/controller/cart.controller.ts
+++ src/cart/controller/cart.controller.ts
@@ -2,13 +2,21 @@ import {
   Body,
   Controller,
   Delete,
+  ForbiddenException,
   Get,
   Param,
   Patch,
+  UnauthorizedException,
   UseGuards,
 } from '@nestjs/common';
+import {
+  ApiForbiddenResponse,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 import { User } from '@prisma/client';
-import { Public } from '../../auth/decorators/public.decorator';
 import { Roles } from '../../auth/decorators/roles.decorator';
 import { GetUser } from '../../auth/decorators/user.decorator';
 import { JwtAuthGuard } from '../../auth/guards/jwt-guard';
@@ -18,6 +26,7 @@ import { CartDto } from '../models/cart.dto';
 import { CreateCartItemDto } from '../models/create-cart-item.dto';
 import { CartService } from '../service/cart.service';
 
+@ApiTags('Cart')
 @Controller('cart')
 export class CartController {
   constructor(private readonly cartService: CartService) {}
@@ -25,6 +34,23 @@ export class CartController {
   @Get('')
   @Roles(Role.USER)
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: 'Get all item in Cart' })
+  @ApiResponse({
+    status: 200,
+    description: 'Return a list of items',
+  })
+  @ApiUnauthorizedResponse({
+    schema: {
+      example: new UnauthorizedException().getResponse(),
+    },
+    description: 'Item is not logged in',
+  })
+  @ApiForbiddenResponse({
+    description: 'User is not authorized get all items in a Cart',
+    schema: {
+      example: new ForbiddenException().getResponse(),
+    },
+  })
   async getAllItems(@GetUser() user: User): Promise<CartItemsDto[]> {
     return await this.cartService.getItems(user.id);
   }
@@ -32,6 +58,23 @@ export class CartController {
   @Patch('/cart-item')
   @Roles(Role.USER)
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: 'Added cart items in Cart' })
+  @ApiResponse({
+    status: 200,
+    description: 'Return list of cart items',
+  })
+  @ApiUnauthorizedResponse({
+    schema: {
+      example: new UnauthorizedException().getResponse(),
+    },
+    description: 'User is not logged in',
+  })
+  @ApiForbiddenResponse({
+    description: 'User is not authorized to create or update items in Cart',
+    schema: {
+      example: new ForbiddenException().getResponse(),
+    },
+  })
   async updateItem(
     @GetUser() user: User,
     @Body() createCartItem: CreateCartItemDto,
@@ -42,6 +85,18 @@ export class CartController {
   @Delete('/cart-item/:id')
   @Roles(Role.ADMIN)
   @UseGuards(JwtAuthGuard)
+  @ApiUnauthorizedResponse({
+    schema: {
+      example: new UnauthorizedException().getResponse(),
+    },
+    description: 'User is not logged in',
+  })
+  @ApiForbiddenResponse({
+    description: 'User is not authorized to delete this dessert',
+    schema: {
+      example: new ForbiddenException().getResponse(),
+    },
+  })
   async delete(@GetUser() user: User, @Param('id') id: number): Promise<void> {
     await this.cartService.delete(user.id, id);
   }
