9bc3e4dc79165b4b2c7742089a3b367526129068
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-05-17 19:50:38 -0500

    fix: Removed unused file

--- src/desserts/models/pagination-desserts.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsOptional } from 'class-validator';
-import { PaginationOptions } from 'src/utils/pagination-options.dto';
-
-export class PaginationOptionsDessert extends PaginationOptions {
-  @IsOptional()
-  category: string = null;
-}
