6866b54626f5b61a1719997e3cc4aa81056d83a4
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-05-17 19:54:09 -0500

    feat: Added decorators in password property

--- src/users/models/create-user.dto.ts
+++ src/users/models/create-user.dto.ts
@@ -6,6 +6,9 @@ import {
   IsOptional,
   IsString,
   Length,
+  Matches,
+  MaxLength,
+  MinLength,
 } from 'class-validator';
 
 export class CreateUserDto {
@@ -27,7 +30,24 @@ export class CreateUserDto {
   readonly email: string;
 
   @IsString()
-  @Length(8, 20)
+  @MinLength(8, {
+    message: 'PASSWORD_MIN_LENGTH: 8',
+  })
+  @MaxLength(16, {
+    message: 'PASSWORD_MAX_LENGTH: 16',
+  })
+  @Matches(/\d/, {
+    message: 'PASSWORD_MISSING: NUMBER',
+  })
+  @Matches(/[A-Z]/, {
+    message: 'PASSWORD_MISSING: UPPER_CASE_LETTER',
+  })
+  @Matches(/[a-z]/, {
+    message: 'PASSWORDS_MISSING: LOWER_CASE_LETTER',
+  })
+  @Matches(/[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/, {
+    message: 'PASSWORDS_MISSING: SPECIAL_CHARACTER',
+  })
   readonly password: string;
 
   @IsNotEmpty()
