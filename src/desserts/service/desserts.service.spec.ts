4fc573279adb80d0b965858d2454e0862def91d2
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-05-17 23:25:12 -0500

    feat: Added initial code in desserts tests files

--- src/desserts/service/desserts.service.spec.ts
+++ src/desserts/service/desserts.service.spec.ts
@@ -1,18 +1,81 @@
+import faker from '@faker-js/faker';
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Category } from '@prisma/client';
+import { AuthService } from '../../auth/service/auth.service';
+import { CategoryFactory } from '../../categories/factories/category.factory';
+import { PrismaService } from '../../prisma.service';
+import { DessertFactory } from '../factories/dessert.factory';
+import { CreateDessertDto } from '../models/create-dessert.dto';
+
 import { DessertsService } from './desserts.service';
 
-describe('DessertsService', () => {
-  let service: DessertsService;
+jest.spyOn(console, 'error').mockImplementation(jest.fn());
 
-  beforeEach(async () => {
+describe('DessertsService', () => {
+  let serviceDessert: DessertsService;
+  let prisma: PrismaService;
+
+  let dessertFactory: DessertFactory;
+  let categoryFactory: CategoryFactory;
+
+  let categories: Category[];
+  let dessert: CreateDessertDto;
+
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [DessertsService],
+      providers: [DessertsService, PrismaService, AuthService],
     }).compile();
 
-    service = module.get<DessertsService>(DessertsService);
+    prisma = module.get<PrismaService>(PrismaService);
+    serviceDessert = module.get<DessertsService>(DessertsService);
+
+    dessertFactory = new DessertFactory(prisma);
+    categoryFactory = new CategoryFactory(prisma);
+
+    categories = await categoryFactory.makeMany(5);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  beforeEach(() => {
+    dessert = {
+      name: faker.datatype.string(40),
+      description: faker.datatype.string(255),
+      price: faker.datatype.float(),
+      stock: faker.datatype.number(),
+      categoryId: faker.datatype.number(),
+    };
+  });
+
+  afterAll(async () => {
+    await prisma.clearDB();
+    await prisma.$disconnect();
+  });
+
+  describe('findOne', () => {
+    let category: Category;
+    beforeAll(async () => {
+      // jest.mock('jsonwebtoken', () => ({
+      //   sign: jest.fn().mockImplementation(() => 'my.jwt.token'),
+      // }));
+      category = await categoryFactory.make();
+    });
+
+    it('should return dessert by Id category', async () => {
+      const createDessert = await dessertFactory.make({
+        category: {
+          connect: { id: category.id },
+        },
+      });
+      const result = await serviceDessert.findOne(createDessert.id);
+      expect(result).toHaveProperty('name', createDessert.name);
+      expect(result).toHaveProperty('description', createDessert.description);
+      expect(result).toHaveProperty('price', createDessert.price);
+      expect(result).toHaveProperty('stock', createDessert.stock);
+    });
+    it("should throw a error if product doesn't exist", async () => {
+      await expect(serviceDessert.findOne(1000)).rejects.toThrow(
+        new NotFoundException("Product doesn't exist"),
+      );
+    });
   });
 });
