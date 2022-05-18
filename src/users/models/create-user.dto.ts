110b8dbcdcc002afe97daf02c0ff843ac268358e
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-05-17 20:05:22 -0500

    docs: Added swagger documentation in user dtos

--- src/users/models/create-user.dto.ts
+++ src/users/models/create-user.dto.ts
@@ -1,34 +1,58 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Role } from '@prisma/client';
 import {
   IsEmail,
+  IsIn,
   IsLowercase,
   IsNotEmpty,
   IsOptional,
   IsString,
-  Length,
   Matches,
   MaxLength,
   MinLength,
 } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({
+    name: 'firstName',
+    description: 'Firstname of User',
+    example: 'Maria',
+  })
   @IsNotEmpty()
   @IsString()
   @IsOptional()
   readonly firstName: string;
 
+  @ApiProperty({
+    name: 'lastName',
+    description: 'Firstname of User',
+    example: 'Luque',
+  })
   @IsNotEmpty()
   @IsString()
   @IsOptional()
   readonly lastName: string;
 
+  @ApiProperty({
+    name: 'userNAme',
+    description: 'Username of User',
+    example: 'mariCarmen',
+  })
   @IsNotEmpty()
   @IsString()
   readonly userName: string;
 
+  @ApiProperty({
+    description: 'Email of user',
+    type: String,
+  })
   @IsEmail()
   readonly email: string;
 
+  @ApiProperty({
+    description: 'Password of user',
+    type: String,
+  })
   @IsString()
   @MinLength(8, {
     message: 'PASSWORD_MIN_LENGTH: 8',
@@ -50,6 +74,7 @@ export class CreateUserDto {
   })
   readonly password: string;
 
+  @IsIn([Role.ADMIN, Role.USER])
   @IsNotEmpty()
   @IsLowercase()
   readonly role: Role;
