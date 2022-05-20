b9e052010add56348e1df81d911d341bb065287a
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-05-20 09:28:48 -0500

    fix: Resolved circular dependency in app module and users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,19 +6,18 @@ import { AuthModule } from './auth/auth.module';
 import { DessertsModule } from './desserts/desserts.module';
 import { CategoriesModule } from './categories/categories.module';
 import { CartModule } from './cart/cart.module';
-import { LikesService } from './likes/likes.service';
 import { LikesModule } from './likes/likes.module';
 
 @Module({
   imports: [
-    UsersModule,
     AuthModule,
+    UsersModule,
     CategoriesModule,
     DessertsModule,
     CartModule,
     LikesModule,
   ],
   controllers: [AppController],
-  providers: [AppService, LikesService],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,14 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { JwtAuthGuard } from '../auth/guards/jwt-guard';
 import { RolesGuard } from '../auth/guards/roles.guard';
-import { AuthModule } from '../auth/auth.module';
 import { PrismaService } from '../prisma.service';
 import { UsersController } from './controller/users.controller';
 import { UsersService } from './service/users.service';
 
 @Module({
-  imports: [AuthModule, ConfigModule],
   controllers: [UsersController],
   providers: [UsersService, PrismaService, JwtAuthGuard, RolesGuard],
   exports: [UsersService],
