fca97ddd682970895454a6dbd44e5e261cfc82b1
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-05-20 09:29:12 -0500

    feat: Added providers in likes modules

--- src/likes/likes.module.ts
+++ src/likes/likes.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/guards/jwt-guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { PrismaService } from '../prisma.service';
 import { LikesService } from './service/likes.service';
 
 @Module({
-  providers: [LikesService],
+  providers: [LikesService, PrismaService, JwtAuthGuard, RolesGuard],
+  exports: [LikesService],
 })
 export class LikesModule {}
