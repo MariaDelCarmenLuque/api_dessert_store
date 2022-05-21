1834a2237ff2b1a8b63f76fd9f58d2079b1b22f6
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-05-21 06:56:37 -0500

    feat: Added order service as provider in cart module

--- src/cart/cart.module.ts
+++ src/cart/cart.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from 'src/auth/auth.module';
 import { DessertsModule } from 'src/desserts/desserts.module';
+import { OrdersService } from 'src/orders/service/orders.service';
 import { PrismaService } from 'src/prisma.service';
 import { CartController } from './controller/cart.controller';
 import { CartService } from './service/cart.service';
@@ -8,6 +9,6 @@ import { CartService } from './service/cart.service';
 @Module({
   imports: [AuthModule, DessertsModule],
   controllers: [CartController],
-  providers: [CartService, PrismaService],
+  providers: [CartService, PrismaService, OrdersService],
 })
 export class CartModule {}
