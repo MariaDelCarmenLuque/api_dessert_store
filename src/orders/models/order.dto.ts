90bc4b8d6b82f0cd51aa16b330e83536977fa3a7
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-05-21 06:55:57 -0500

    feat: Updated orderDto

--- src/orders/models/order.dto.ts
+++ src/orders/models/order.dto.ts
@@ -1,17 +1,34 @@
-import { CartItem } from '@prisma/client';
+import faker from '@faker-js/faker';
+import { ApiProperty } from '@nestjs/swagger';
+import { OrderItem } from '@prisma/client';
 import { Exclude, Expose } from 'class-transformer';
 
 @Exclude()
 export class OrderDto {
+  @ApiProperty({
+    description: 'uuid order',
+    example: faker.datatype.uuid(),
+  })
   @Expose()
   uuid: string;
 
+  @ApiProperty({
+    description: 'total Price of a order',
+    example: faker.datatype.number(),
+  })
   @Expose()
   totalPrice: number;
 
+  @ApiProperty({
+    description: 'Date of create a order',
+    example: faker.datatype.datetime(),
+  })
   @Expose()
   createdAt: Date;
 
+  @ApiProperty({
+    description: 'List od OrderItems',
+  })
   @Expose()
-  items: CartItem[];
+  items: OrderItem[];
 }
