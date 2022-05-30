67a37fdfc1b3b2db9ee1b35dc27b2b29d3699c78
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-05-30 09:26:08 -0500

    docs: Added comments with description and examples in all properties of Cart and cartItems dtos

--- src/cart/dtos/request/create-cart-item.dto.ts
+++ src/cart/dtos/request/create-cart-item.dto.ts
@@ -1,9 +1,17 @@
 import { IsInt, IsNumber, IsPositive } from 'class-validator';
 
 export class CreateCartItemDto {
+  /**
+   * Id of dessert
+   * @example 1
+   */
   @IsNumber()
   readonly dessertId: number;
 
+  /**
+   * Quantity od Desserts
+   * @example 10
+   */
   @IsPositive()
   @IsInt()
   readonly quantity: number;
--- src/cart/dtos/response/cart-item.dto.ts
+++ src/cart/dtos/response/cart-item.dto.ts
@@ -3,29 +3,60 @@ import { Exclude, Expose, Transform } from 'class-transformer';
 
 @Exclude()
 export class CartItemsDto {
+  /**
+   * Quantity of Items
+   * @example 3
+   */
   @Expose()
   readonly quantity: number;
 
+  /**
+   * Unit price of Item
+   * @example 12.5
+   */
   @Expose()
   readonly unitPrice: number;
 
+  /**
+   * Total Price of all items
+   * @example 3
+   */
   @Expose()
   readonly totalPrice: number;
 
+  /**
+   * Id of Cart
+   * @example 3
+   */
   @Expose()
   readonly cartId: number;
 
+  /**
+   * Dessert
+   */
   @Expose()
   readonly dessert: Dessert;
 
+  /**
+   * CartItem's delete date
+   * @example '2016-03-26 10:10:10-05:00'
+   */
   @Expose()
   @Transform(({ value }) => value?.toISOString())
-  readonly deletedAt: Date;
+  readonly deletedAt?: Date;
 
+  /**
+   * CartItem's update date
+   * @example '2016-03-26 10:10:10-05:00'
+   */
   @Expose()
   @Transform(({ value }) => value?.toISOString())
   readonly updatedAt: string;
 
+  /**
+   * CartItem's creation date
+   * @example '2016-03-26 10:10:10-05:00'
+   */
   @Expose()
   @Transform(({ value }) => value?.toISOString())
   readonly createdAt: string;
