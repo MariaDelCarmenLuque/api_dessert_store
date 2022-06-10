05b7ccaf66582a3540f2b87a84f73541008ae424
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-06-10 09:20:52 -0500

    feat: Added createImage method in Dessert Resolver

--- src/desserts/resolvers/dessert.resolver.ts
+++ src/desserts/resolvers/dessert.resolver.ts
@@ -1,5 +1,6 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CreateDessertInput } from '../dtos/input/create-dessert.input';
+import { ImageInput } from '../dtos/input/create-image.input';
 import { UpdateDessertInput } from '../dtos/input/update-dessert.input';
 import { Dessert } from '../models/dessert.model';
 import { DessertsService } from '../service/desserts.service';
@@ -26,8 +27,16 @@ export class DessertsResolver {
     return await this.dessertService.updateDessert(id, updateDessertInput);
   }
 
-  @Mutation(() => Dessert, { description: 'Update status Dessert' })
+  @Mutation(() => Dessert, { description: 'Mutation: Update status Dessert' })
   async updateStatusDessert(@Args('id') id: number) {
     return await this.dessertService.updateStatus(id);
+  }
+
+  @Mutation(() => Dessert, { description: 'Mutation: Create a Image' })
+  async createImage(
+    @Args('id') id: number,
+    @Args('imageInput') imageInput: ImageInput,
+  ) {
+    return await this.dessertService.createImage(id, imageInput);
   }
 }
