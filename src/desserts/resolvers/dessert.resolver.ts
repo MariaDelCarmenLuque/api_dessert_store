84cc26fe08d1e373988da7b67071233eb72ef76d
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-06-10 08:42:47 -0500

    feat: Added updateDessert and updateStatus methods in dessert resolver

--- src/desserts/resolvers/dessert.resolver.ts
+++ src/desserts/resolvers/dessert.resolver.ts
@@ -1,5 +1,6 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
-import { DessertInput } from '../dtos/input/create-dessert.input';
+import { CreateDessertInput } from '../dtos/input/create-dessert.input';
+import { UpdateDessertInput } from '../dtos/input/update-dessert.input';
 import { Dessert } from '../models/dessert.model';
 import { DessertsService } from '../service/desserts.service';
 
@@ -8,12 +9,25 @@ export class DessertsResolver {
   constructor(private dessertService: DessertsService) {}
 
   @Query(() => Dessert, { description: 'Query: Return a dessert by ID' })
-  async productGetOne(@Args('id') id: number) {
+  async dessertGetOne(@Args('id') id: number) {
     return await this.dessertService.findOne(id);
   }
 
   @Mutation(() => Dessert, { description: 'Mutation: Create a Dessert' })
-  async createDessert(@Args('dessertInput') dessertInput: DessertInput) {
+  async createDessert(@Args('dessertInput') dessertInput: CreateDessertInput) {
     return await this.dessertService.create(dessertInput);
+  }
+
+  @Mutation(() => Dessert, { description: 'Mutation: Update a Dessert' })
+  async updateDessert(
+    @Args('id') id: number,
+    @Args('updateDessertInput') updateDessertInput: UpdateDessertInput,
+  ) {
+    return await this.dessertService.updateDessert(id, updateDessertInput);
+  }
+
+  @Mutation(() => Dessert, { description: 'Update status Dessert' })
+  async updateStatusDessert(@Args('id') id: number) {
+    return await this.dessertService.updateStatus(id);
   }
 }
