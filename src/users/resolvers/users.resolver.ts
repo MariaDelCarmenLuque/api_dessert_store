d44a1320307c29e2f2165cc61a3296a46be41a64
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-06-10 10:54:16 -0500

    feat: Added userGetAll and updateUser mutations in Users resolver

--- src/users/resolvers/users.resolver.ts
+++ src/users/resolvers/users.resolver.ts
@@ -1,4 +1,5 @@
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { UpdateUserInput } from '../dtos/input/update-user.input';
 import { User } from '../models/user.model';
 import { UsersService } from '../service/users.service';
 
@@ -6,8 +7,21 @@ import { UsersService } from '../service/users.service';
 export class UsersResolver {
   constructor(private usersService: UsersService) {}
 
-  @Query(() => User)
+  @Query(() => User, { description: 'Return a User by Id' })
   async userGetOne(@Args('id') id: number) {
     return await this.usersService.findOne(id);
+  }
+
+  @Query(() => [User], { description: 'Query: Return all users' })
+  async userGetAll() {
+    return await this.usersService.getAll();
+  }
+
+  @Mutation(() => User, { description: 'Mutation: Update a User' })
+  async updateUser(
+    @Args('id') id: number,
+    @Args('updateDessertInput') updateUserInput: UpdateUserInput,
+  ) {
+    return await this.usersService.updateUser(id, updateUserInput);
   }
 }
