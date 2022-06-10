4a2ce7b81eaf047555c2e35a1e012a192bcad7b8
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-06-10 14:58:22 -0500

    feat: Added readonly in all properties in create user input

--- src/users/dtos/input/create-user.input.ts
+++ src/users/dtos/input/create-user.input.ts
@@ -1,35 +1,60 @@
 import { Field, ObjectType } from '@nestjs/graphql';
-import { Role } from '@prisma/client';
-import { IsNotEmpty, IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { Role } from 'src/auth/roles.enum';
 
 @ObjectType({ description: 'User information to create a User' })
 export class CreateUserInput {
-  @Field({ description: "User's Role" })
+  @Field(() => Role, { description: "User's Role" })
   @IsNotEmpty()
-  role: Role;
+  readonly role: Role;
 
   @Field({ description: "User's first name" })
   @IsNotEmpty()
   @IsString()
-  firstName: string;
+  readonly firstName: string;
 
   @Field({ description: "User's last name" })
   @IsNotEmpty()
   @IsString()
-  lastName: string;
+  readonly lastName: string;
 
   @Field({ description: "User's username" })
   @IsNotEmpty()
   @IsString()
-  userName: string;
+  readonly userName: string;
 
   @Field({ description: "User's email" })
   @IsNotEmpty()
-  @IsString()
-  email: string;
+  @IsEmail()
+  readonly email: string;
 
   @Field({ description: "User's password" })
   @IsNotEmpty()
   @IsString()
-  password: string;
+  @MinLength(8, {
+    message: 'PASSWORD_MIN_LENGTH: 8',
+  })
+  @MaxLength(16, {
+    message: 'PASSWORD_MAX_LENGTH: 16',
+  })
+  @Matches(/\d/, {
+    message: 'PASSWORD_MISSING: NUMBER',
+  })
+  @Matches(/[A-Z]/, {
+    message: 'PASSWORD_MISSING: UPPER_CASE_LETTER',
+  })
+  @Matches(/[a-z]/, {
+    message: 'PASSWORDS_MISSING: LOWER_CASE_LETTER',
+  })
+  @Matches(/[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/, {
+    message: 'PASSWORDS_MISSING: SPECIAL_CHARACTER',
+  })
+  readonly password: string;
 }
