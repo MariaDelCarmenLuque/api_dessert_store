9a719c9d668c72268f8d130ca64242036b42e4d6
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-06-10 20:24:49 -0500

    feat: Added all endpoints in category resolver file

---
+++ src/categories/resolvers/category.resolver.ts
@@ -0,0 +1,35 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateCategoryInput } from '../dtos/input/create-category.input';
+import { UpdateCategoryInput } from '../dtos/input/update-category.input';
+import { Category } from '../models/category.model';
+import { CategoriesService } from '../service/categories.service';
+
+@Resolver()
+export class CategoriesResolver {
+  constructor(private categoriesService: CategoriesService) {}
+
+  @Query(() => [Category], { description: 'Query: Return all categories' })
+  async categoriesGetAll() {
+    return await this.categoriesService.getAll();
+  }
+
+  @Query(() => Category, { description: 'Query: Return a category by Id' })
+  async categoryGetOne(@Args('id') id: number) {
+    return await this.categoriesService.findOne(id);
+  }
+
+  @Mutation(() => Category, { description: 'Mutation: Create a Category' })
+  async createCategory(
+    @Args('createCategoryInput') createCategoryInput: CreateCategoryInput,
+  ) {
+    return await this.categoriesService.create(createCategoryInput);
+  }
+
+  @Mutation(() => Category, { description: 'Mutation: Update a Category' })
+  async updateCategory(
+    @Args('id') id: number,
+    @Args('updateCategoryInput') updateCategoryInput: UpdateCategoryInput,
+  ) {
+    return await this.categoriesService.updateCategory(id, updateCategoryInput);
+  }
+}
