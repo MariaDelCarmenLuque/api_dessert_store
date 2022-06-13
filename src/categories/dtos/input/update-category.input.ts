bf2f3b4ca7c000046d94e9d1a757a4ee2d7defed
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-06-13 15:46:53 -0500

    fix: Added InputType decorator into Update Category Input

--- src/categories/dtos/input/update-category.input.ts
+++ src/categories/dtos/input/update-category.input.ts
@@ -1,4 +1,7 @@
-import { PartialType } from '@nestjs/swagger';
+import { InputType, PartialType } from '@nestjs/graphql';
 import { CreateCategoryInput } from './create-category.input';
 
+@InputType({
+  description: 'Category information to update a Category',
+})
 export class UpdateCategoryInput extends PartialType(CreateCategoryInput) {}
