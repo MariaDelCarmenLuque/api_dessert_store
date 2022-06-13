1feec1d52168532d13eb40e58396066240e6572a
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-06-13 18:42:35 -0500

    feat: Added Order Resolver into providers in Order Module

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -3,11 +3,12 @@ import { AuthModule } from 'src/auth/auth.module';
 import { DessertsModule } from 'src/desserts/desserts.module';
 import { PrismaService } from 'src/prisma.service';
 import { OrdersController } from './controller/orders.controller';
+import { OrdersResolver } from './resolvers/order.resolver';
 import { OrdersService } from './service/orders.service';
 
 @Module({
   imports: [DessertsModule, AuthModule],
   controllers: [OrdersController],
-  providers: [OrdersService, PrismaService],
+  providers: [OrdersService, PrismaService, OrdersResolver],
 })
 export class OrdersModule {}
