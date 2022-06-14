ee6b0a42f9cca422d34a99b2ba31b460e025f036
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-06-14 10:36:25 -0500

    feat: Added userSeed and dessertSeed into main function

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,16 @@
 import { PrismaClient } from '@prisma/client';
 import { categorySeed } from './seeds/category.seed';
+import { dessertSeed } from './seeds/dessert.seed';
+import { userSeed } from './seeds/user.seed';
 
 const prisma = new PrismaClient();
 
 async function main() {
-  await Promise.all([await categorySeed(prisma)]);
+  await Promise.all([
+    await categorySeed(prisma),
+    await userSeed(prisma),
+    await dessertSeed(prisma),
+  ]);
 }
 main()
   .catch((e) => {
