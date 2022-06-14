911a6915ed65a24afa46eb9daa074374f103723a
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-06-14 15:18:46 -0500

    feat: Addes delete User endpoint and renamed all endpoints into User Resolver

--- src/users/resolvers/users.resolver.ts
+++ src/users/resolvers/users.resolver.ts
@@ -7,21 +7,38 @@ import { UsersService } from '../service/users.service';
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
 
-  @Query(() => User, { description: 'Return a User by Id' })
-  async userGetOne(@Args('id') id: number) {
+  @Query(() => User, {
+    description: 'Query: Return a User by Id',
+    name: 'UserGetOne',
+  })
+  async getOneUser(@Args('id') id: number) {
     return await this.usersService.findOne(id);
   }
 
-  @Query(() => [User], { description: 'Query: Return all users' })
-  async userGetAll() {
+  @Query(() => [User], {
+    description: 'Query: Return all users',
+    name: 'UserGetAll',
+  })
+  async getAllUser() {
     return await this.usersService.getAll();
   }
 
-  @Mutation(() => User, { description: 'Mutation: Update a User' })
+  @Mutation(() => User, {
+    description: 'Mutation: Update a User',
+    name: 'UserUpdate',
+  })
   async updateUser(
     @Args('id') id: number,
     @Args('updateDessertInput') updateUserInput: UpdateUserInput,
   ) {
     return await this.usersService.updateUser(id, updateUserInput);
+  }
+
+  @Mutation(() => User, {
+    description: 'Mutation: Delete a User',
+    name: 'UserDelete',
+  })
+  async deleteUser(@Args('id') id: number) {
+    return await this.usersService.delete(id);
   }
 }
