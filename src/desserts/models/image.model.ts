f265c7160aac90b27facf5fa89cc1ca82cc0aebf
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-06-15 07:11:20 -0500

    feat: Added properties into image model

--- src/desserts/models/image.model.ts
+++ src/desserts/models/image.model.ts
@@ -2,9 +2,17 @@ import { Field, ObjectType } from '@nestjs/graphql';
 
 @ObjectType()
 export class Image {
-  @Field({ description: 'Image id' })
   readonly id: number;
+
+  @Field({ description: "Image's uuid" })
+  readonly uuid: string;
 
   @Field({ description: "Image's name" })
   readonly name: string;
+
+  @Field({ description: "Image's updated date " })
+  readonly updatedAt: Date;
+
+  @Field({ description: "Image's created date " })
+  readonly createdAt: Date;
 }
