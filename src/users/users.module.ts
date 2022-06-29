9bca3dab3694a4622b2205382e9228cf2dc771f7
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-06-29 16:25:20 -0500

    feat: Added users resolver into providers in user module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,11 +3,18 @@ import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { PrismaService } from '../prisma.service';
 import { UsersController } from './controller/users.controller';
+import { UsersResolver } from './resolvers/users.resolver';
 import { UsersService } from './service/users.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, PrismaService, JwtAuthGuard, RolesGuard],
+  providers: [
+    UsersService,
+    PrismaService,
+    JwtAuthGuard,
+    RolesGuard,
+    UsersResolver,
+  ],
   exports: [UsersService],
 })
 export class UsersModule {}
