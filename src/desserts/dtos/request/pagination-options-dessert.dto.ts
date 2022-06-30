0d3df2ec040fd62baf4daba24db6faffcd142a70
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-06-30 15:18:18 -0500

    fix: Remove default decorator and Is Positive decorator in all properties into pagination options dto

---
+++ src/desserts/dtos/request/pagination-options-dessert.dto.ts
@@ -0,0 +1,24 @@
+import { IsNumber, IsOptional } from 'class-validator';
+
+export class PaginationOptionsDessertDto {
+  /**
+   * Number of page
+   */
+  @IsOptional()
+  @IsNumber()
+  readonly page?: number = 1;
+
+  /**
+   * Quantity of desserts per page
+   */
+  @IsOptional()
+  @IsNumber()
+  readonly take?: number = 10;
+
+  /**
+   * Category's Id of Dessert
+   */
+  @IsOptional()
+  @IsNumber()
+  readonly categoryId?: number | null = null;
+}
