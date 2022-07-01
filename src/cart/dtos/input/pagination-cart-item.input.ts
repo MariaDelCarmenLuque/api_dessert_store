d1d5df2804755a39786bf1a7ae1e2496f65f141c
MariaDelCarmenLuque <maritcarmn@gmail.com>
2022-07-01 09:15:42 -0500

    feat: Added Cart Item pagination Input and Dto

---
+++ src/cart/dtos/input/pagination-cart-item.input.ts
@@ -0,0 +1,15 @@
+import { Field, InputType } from '@nestjs/graphql';
+import { IsNumber, IsOptional } from 'class-validator';
+
+@InputType({})
+export class PaginationOptionsCartItemInput {
+  @Field({ nullable: true, description: 'Quantity of item per page' })
+  @IsOptional()
+  @IsNumber()
+  readonly take?: number = 10;
+
+  @Field({ nullable: true, description: 'Number of page' })
+  @IsOptional()
+  @IsNumber()
+  readonly page?: number = 1;
+}
